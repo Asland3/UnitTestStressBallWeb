8da1e447d21eb1c19d8cc7562b94b1a841c600b3
Asland3 <malthe3@outlook.com>
2022-05-12 11:57:31 +0200

    test

---
+++ UnitTestStressBallWeb/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.ObjectModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace UnitTestStressBallWeb
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private static readonly string DriverDirectory = "/Users/malthethomsen/Downloads";
+        private static IWebDriver _driver;
+        
+        [ClassInitialize]
+        public static void Setup(TestContext context)
+        {
+             _driver = new ChromeDriver(DriverDirectory); 
+        }
+
+        [ClassCleanup]
+        public static void TearDown()
+        {
+            _driver.Dispose();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //string url = "http://localhost:3000";
+            _driver.Navigate().GoToUrl("http://127.0.0.1:5501/nytkast.html");
+            
+            string title = _driver.Title;
+            Assert.AreEqual("Stressball - Nyt Kast", title);
+            
+            IWebElement buttonElement = _driver.FindElement(By.Id("button"));
+            buttonElement.Click();
+        }
+        
+    }
+}
+
+
